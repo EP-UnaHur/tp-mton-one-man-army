65bde8bbc9c6561cdb8772de0f5aa0da26d4a358
Lucas Julian Guerci <lucas.guerci@gmail.com>
2024-06-03 18:41:05 -0300

    updated readme

---
+++ migrations/20240603210502-create-carrera.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Carreras', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      grado: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      universidad: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Carreras');
+  }
+};
