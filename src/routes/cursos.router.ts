c404f4a6b4abd95e6c0b1a3fd26d2b61fccd5180
Lucas Julian Guerci <lucas.guerci@gmail.com>
2024-06-03 22:47:29 -0300

    base cursos endpoint

--- src/routes/cursos.router.ts
+++ src/routes/cursos.router.ts
@@ -1,21 +1,67 @@
 import { Router, Request, Response } from 'express'
+import CursosController from '../controllers/cursos.controller'
+import handlePromise from '../utils/promise'
 
 const router = Router()
 
 router.route("/cursos")
-        .get((req:Request,res:Response)=>{
-            
+        .get(async (_:Request,res:Response)=>{
+                const [response, err] = await handlePromise(CursosController.getAll())
+
+                if(err) {
+                        console.error(err)
+                        return res.status(err.status).send(err.error)
+                }
+
+                res.status(response.status).send(response.body)
         })
 
 router.route("/cursos/:id")
-        .get((req:Request,res:Response)=>{
+        .get(async (req:Request,res:Response)=>{
 
+                const id = parseInt(req.params.id)
+
+                if(isNaN(id)) return res.status(400).send("El id de curso debe ser numerico")
+
+                const [response, err] = await handlePromise(CursosController.getSingle(id))  
+
+                if(err) {
+                        console.error(err)
+                        return res.status(err.status).send(err.error)
+                }
+
+                res.status(response.status).send(response.body)
         })
-        .delete((req:Request,res:Response)=>{
-            
+        .delete(async(req:Request,res:Response)=>{
+        const id = parseInt(req.params.id)
+
+        if(isNaN(id)) return res.status(400).send("El id de curso debe ser numerico")
+
+        const [response, err] = await handlePromise(CursosController.deleteById(id))  
+
+        if(err) {
+                console.error(err)
+                return res.status(err.status).send(err.error)
+        }
+
+        res.sendStatus(response.status)
         })
-        .put((req:Request,res:Response)=>{
-            
+        .put(async(req:Request,res:Response)=>{
+                const id = parseInt(req.params.id)
+                if(isNaN(id)) return res.status(400).send("El id de curso debe ser numerico")
+
+                const {comision,turno,fechaInicio,fechaFin} = req.body
+
+                const [response, err] = await handlePromise(
+                        CursosController.updateById(id,{comision,turno,fechaInicio,fechaFin})
+                )  
+
+                if(err) {
+                        console.error(err)
+                        return res.status(err.status).send(err.error)
+                }
+
+                res.status(response.status).send(response.body)
         })
 
 router.route("/cursos/:id/profesores")
